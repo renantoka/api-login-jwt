b4611dae0e4ad7dd68cf8e5330431d8dcb5e53ed
Renan Tokashiki <renantoka@icloud.com>
2021-06-04 16:10:40 -0400

    added JWT route protection for products and orders

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require('multer');
 
+const checkAuth = require('../middleware/check-auth');
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads/');
@@ -67,7 +69,7 @@ router.get("/", (req, res, next) => {
         });
 });
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -126,7 +128,7 @@ router.get("/:productId", (req, res, next) => {
         });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch("/:productId", checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -151,7 +153,7 @@ router.patch("/:productId", (req, res, next) => {
         });
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete("/:productId", checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
         .exec()
